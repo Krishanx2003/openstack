d437fc37034f05e41ce6bfd2a993b39872499777
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-03-17 13:20:06 +0530

    add clerk auth

---
+++ components/Navbar.tsx
@@ -0,0 +1,41 @@
+
+// components/Navbar.tsx
+
+import Link from 'next/link';
+import styles from './Navbar.module.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link href="/">
+          Logo
+        </Link>
+      </div>
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="/">
+          =Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link href="/services">
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link href="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
