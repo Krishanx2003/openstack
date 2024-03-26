c16fbd7246079fbb65c0fb17e295afd564ca7a7f
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-03-26 15:08:28 +0530

    middleware

--- next.config.js
+++ next.config.js
@@ -1,9 +1,26 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental : {
-        serverActions:true,
-        mdxRs:true,
-        serverComponentsExternalPackages:['mongoose']
-    }
-};
-
-module.exports = nextConfig;
+    experimental: {
+      serverActions: true,
+      mdxRs: true,
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "*",
+        },
+        {
+          protocol: "http",
+          hostname: "*",
+        },
+      ],
+    },
+    compiler: {
+      removeConsole: process.env.NODE_ENV === "production",
+    },
+  };
+  
+  module.exports = nextConfig;
+  
