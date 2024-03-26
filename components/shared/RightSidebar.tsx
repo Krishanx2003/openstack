af8f0b62272d1d57f7e169fbe0a306c582f2c848
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-03-26 15:58:03 +0530

    RightSidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,17 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
+import { getTopPopularTags } from "@/lib/actions/tag.actions";
 
 const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div>
         <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
         <div className="mt-7 flex w-full flex-col gap-[30px]">
-          {/* Example Question */}
-          <Link href={'/'} className="flex cursor-pointer items-center justify-between gap-7">
+          {hotQuestions.map((question) => (
+            <Link
+              href={`/question/${question._id}`}
+              key={question._id}
+              className="flex cursor-pointer items-center justify-between gap-7"
+            >
               <p className="body-medium text-dark500_light700">
-                How can I improve my coding skills?
+                {question.title}
               </p>
               <Image
                 src="/assets/icons/chevron-right.svg"
@@ -20,20 +29,22 @@ const RightSidebar = async () => {
                 height={20}
                 className="invert-colors"
               />
-           
-          </Link>
-          {/* Add more questions here if needed */}
+            </Link>
+          ))}
         </div>
       </div>
       <div className="mt-16">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-          {/* Example Tag */}
-          <Link href="/tags/javascript"  className="text-dark500_light700 hover:text-primary_dark transition-colors duration-300">
-              #javascript
-           
-          </Link>
-          {/* Add more tags here if needed */}
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.numberOfQuestions}
+              showCount
+            />
+          ))}
         </div>
       </div>
     </section>
